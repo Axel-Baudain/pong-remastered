9626a26b09e3e40cba77709b8e6e3f824a0e69cd
Axel-Baudain <axel.baudain@gmail.com>
2020-12-17 12:28:17 +0100

    Added displayed controls for P1 - P2

--- src/App.js
+++ src/App.js
@@ -20,8 +20,30 @@ function App() {
 
   return (
     <div className="App">
-      <h1 className="main-title"><span className="P1-score">0</span> - <span className="P2-score">0</span></h1>
+      <div className="app-header">
+        <div className="P1__controls">
+          <div>
+            ↑ - Z
+          </div>
+          <div>
+            ↓ - S
+          </div>
+        </div>
+        <div className="centered">
+          <h1 className="main-title"><span className="P1-score">0</span> - <span className="P2-score">0</span></h1>
+          <div className="launch_ball">L - Launch Ball</div>
+        </div>
+        <div className="P2__controls">
+          <div>
+            ↑ - Numpad8
+          </div>
+          <div>
+            ↓ - Numpad5
+          </div>
+        </div>
+      </div>
       <div className="gameboard"></div>
+      <div className="footer">&copy; Axel Baudain - 2020</div>
     </div>
   );
 }
